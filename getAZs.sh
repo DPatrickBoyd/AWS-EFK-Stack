2cc8e30eec4a19380412df14516987ed309ba89c
Patrick Boyd <39223599+DPatrickBoyd@users.noreply.github.com>
2022-05-10 12:54:44 -0700

    added manifest

--- getAZs.sh
+++ getAZs.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
-AZList = $(kubectl get nodes --output="jsonpath={range .items[*]}{.metadata.labels}{'\n'}{end}" | jq '.["topology.kubernetes.io/zone"]')
+AZ_list=($(kubectl get nodes --output="jsonpath={range .items[*]}{.metadata.labels}{end}" | jq -r '.["topology.kubernetes.io/zone"]'))
+node_names=$(kubectl get pods -l app=master -o name -n elastic)
+num=0
+for i in $node_names
+do
+    echo $i
+    echo $num
+    kubectl exec -t -n elastic $i -c elasticsearch -- sh -c "curl -X PUT localhost:9200/_cluster/settings?pretty -H 'Content-Type: application/json' -d'{\"persistent\" :{\"cluster.routing.allocation.awareness.attributes\" :\"zone\"}}'; yq e '.node.attr.zone = \"${AZ_list[$num]}\"' -i '/opt/bitnami/elasticsearch/config/elasticsearch.yml'"
+    echo ${AZ_list[$num]}
+    num=$(($num + 1))
+done
 
-for i in $AZList
-do 
-curl -X PUT "localhost:9200/_cluster/settings?flat_settings=true&pretty" -H 'Content-Type: application/json' -d'
-{
-  "transient" : {
-    "indices.recovery.max_bytes_per_sec" : "20mb"
-  }
-}
-'
